ddfdfc8be0e79d488ee4165d644abbc5d210caac
sukanya <suganya.gnanasekar@in.reportbee.com>
2014-06-20 19:09:32 +0530

    added images

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+	validates_uniqueness_of :firstname, :scope => :lastname
+	validates :email_id,presence: true
+	validates :mobile_number, numericality: {only_integer: true}
+	validates_format_of :email_id, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
